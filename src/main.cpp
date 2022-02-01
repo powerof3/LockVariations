b4ede3fbe722f1bc66195acb678e37eb650612fb
Alan Tse <alandtse@gmail.com>
2022-02-01 00:43:20 -0800

    Revert "Fix ambiguous stl references"
    This reverts commit f9746958cd58bddd54461e5dfc652cd9c0bb0cc6.

--- src/main.cpp
+++ src/main.cpp
@@ -124,24 +124,24 @@ namespace Sound
 	{
 		REL::Relocation<std::uintptr_t> update_lock_angle{ REL::ID(51096) };
 #ifndef SKYRIMVR
-		::stl::write_thunk_call<CylinderSqueak>(update_lock_angle.address() + 0x159);
-		::stl::write_thunk_call<CylinderStop>(update_lock_angle.address() + 0xD9);
-		::stl::write_thunk_call<LockpickingUnlock>(update_lock_angle.address() + 0xA1);
+		stl::write_thunk_call<CylinderSqueak>(update_lock_angle.address() + 0x159);
+		stl::write_thunk_call<CylinderStop>(update_lock_angle.address() + 0xD9);
+		stl::write_thunk_call<LockpickingUnlock>(update_lock_angle.address() + 0xA1);
 #else
-		::stl::write_thunk_call<CylinderSqueak>(update_lock_angle.address() + 0x19E);
-		::stl::write_thunk_call<CylinderStop>(update_lock_angle.address() + 0x11E);
-		::stl::write_thunk_call<LockpickingUnlock>(update_lock_angle.address() + 0xA1);
+		stl::write_thunk_call<CylinderSqueak>(update_lock_angle.address() + 0x19E);
+		stl::write_thunk_call<CylinderStop>(update_lock_angle.address() + 0x11E);
+		stl::write_thunk_call<LockpickingUnlock>(update_lock_angle.address() + 0xA1);
 #endif
 
 		REL::Relocation<std::uintptr_t> rotate_lock{ REL::ID(51098) };
-		::stl::write_thunk_call<CylinderTurn>(rotate_lock.address() + 0x33);
+		stl::write_thunk_call<CylinderTurn>(rotate_lock.address() + 0x33);
 
 		REL::Relocation<std::uintptr_t> update_pick_angle{ REL::ID(51094) };
-		::stl::write_thunk_call<PickMovement>(update_pick_angle.address() +
+		stl::write_thunk_call<PickMovement>(update_pick_angle.address() +
 #ifndef SKYRIMVR
-											  0x13F
+											0x13F
 #else
-											  0x15E
+											0x15E
 #endif
 		);
 	}
