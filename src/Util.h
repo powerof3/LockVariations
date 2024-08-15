414642574369ce8d1b85008852fee8fd1cde7b10
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-08-15 17:15:06 +0530

    Remove leftover BOS code

--- src/Util.h
+++ src/Util.h
@@ -1,12 +1,5 @@
 #pragma once
 
-namespace regex
-{
-	inline srell::regex generic{ R"(\((.*?)\))" };                // pos(0,0,100) -> "0,0,100"
-	inline srell::regex transform{ R"(\((.*?),(.*?),(.*?)\))" };  // pos(0,0,100) -> 0, 0, 100
-	inline srell::regex string{ R"(,\s*(?![^()]*\)))" };          // pos(0, 0, 100), rot(0, 0, 100) -> "pos(0, 0, 100)","rot(0, 0, 100)"
-}
-
 namespace util
 {
 	RE::FormID  GetFormID(const std::string& a_str);
